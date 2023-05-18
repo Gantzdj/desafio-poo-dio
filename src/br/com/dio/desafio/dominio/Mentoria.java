75f15b2a87d0e7975d96d0b17d0927d7d8da010d
Bruno Lima <119960631+Gantzdj@users.noreply.github.com>
2023-05-18 18:00:53 -0300

    first commit

---
+++ src/br/com/dio/desafio/dominio/Mentoria.java
@@ -0,0 +1,41 @@
+package br.com.dio.desafio.dominio;
+
+import java.time.LocalDate;
+public class Mentoria {
+    private String titulo;
+    private String descrição;
+    private LocalDate data;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescrição() {
+        return descrição;
+    }
+
+    public void setDescrição(String descrição) {
+        this.descrição = descrição;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "Mentoria{" +
+                "titulo='" + titulo + '\'' +
+                ", descrição='" + descrição + '\'' +
+                ", data=" + data +
+                '}';
+    }
+}
